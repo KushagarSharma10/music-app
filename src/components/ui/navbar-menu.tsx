946907d1bdb87d31ddbdd21c3a51689bdb23e53d
Kushagar Sharma <sharmakushagar03@gmail.com>
2025-01-28 21:55:01 +0530

    Update navbar-menu.tsx

--- src/components/ui/navbar-menu.tsx
+++ src/components/ui/navbar-menu.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { Children } from "react";
+import React from "react";
 import { motion } from "framer-motion";
 import Link from "next/link";
 import Image from "next/image";
