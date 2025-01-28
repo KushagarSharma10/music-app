5e7f78189957a35a99732e88dcbf051419a7a409
Kushagar Sharma <sharmakushagar03@gmail.com>
2025-01-28 22:47:55 +0530

    Update navbar-menu.tsx

--- src/components/ui/navbar-menu.tsx
+++ src/components/ui/navbar-menu.tsx
@@ -109,7 +109,7 @@ export const ProductItem = ({
   );
 };
 
-export const HoveredLink = ({ children, ...rest }: unknown) => {
+export const HoveredLink = ({ children, ...rest }: React.PropsWithChildren<JSX.IntrinsicElements['a']) => {
   return (
     <Link
       {...rest}
