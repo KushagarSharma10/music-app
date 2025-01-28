57dfe1eba73c895136966af50cdf066475e0c434
Kushagar Sharma <sharmakushagar03@gmail.com>
2025-01-28 21:26:32 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import Image from "next/image";
+
 import HeroSection from "@/components/Herosection";
 import FeaturedCourses from "@/components/FeaturedCourse";
 import MusicSchoolTestimonials from "@/components/TestimonialCards";
